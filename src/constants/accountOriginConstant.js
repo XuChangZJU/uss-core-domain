3562d34a15ad593cfc48b05b488eb3a9c6b4bc18
XuChang <chinawraith@163.com>
2018-03-04 11:39:47 +0800

    增加了一些常量

---
+++ src/constants/accountOriginConstant.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2018/3/4.
+ */
+const initData ={
+    accountOriginGroup: [
+        {
+            id: 1,
+            name: '微信',
+            options: {
+
+            },
+        },
+    ],
+    accountOrigin: [
+        {
+            id: 1,
+            groupId: 1,
+            name: '公众号',
+            options: {
+
+            },
+        },
+        {
+            id: 2,
+            groupId: 1,
+            name: '小程序',
+            options: {
+
+            },
+        }
+    ]
+}
+
+module.exports = {
+    initData,
+};
