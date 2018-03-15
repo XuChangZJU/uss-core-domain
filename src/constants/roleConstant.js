dd42f0bef663c20269d6b7748261ecc076707e47
wangyuef <929077462@qq.com>
2018-03-15 15:34:45 +0800

    增加常量

---
+++ src/constants/roleConstant.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Created by Administrator on 2016/9/9.
+ */
+
+module.exports = {
+    ANYONE: "ANYONE",
+    ROOT: "ROOT",
+    LOGGEDIN: "LOGGEDIN",
+    CLIENT: "CLIENT",
+    EXTERNALUSER: "EXTERNALUSER"
+};
