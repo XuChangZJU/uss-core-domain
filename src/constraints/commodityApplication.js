febc1b321fb84168e3fe55e20a791c4fc2f6e6cf
XuChangZJU <chinawraith@163.com>
2018-03-31 14:09:29 +0800

    增加了constraint

---
+++ src/constraints/commodityApplication.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2018/3/31.
+ */
+const { state: State } = require('../constants/commodityApplicationConstant');
+
+const ParamUpdateMatrix = {
+    params: [
+        State.init, State.inShopCart
+    ],
+    addressId: [
+        State.init, State.unpaid, State.inShopCart, State.paid
+    ],
+    number: [
+        State.init, State.inShopCart
+    ],
+    remark: [
+        State.init, State.unpaid, State.inShopCart, State.paid,
+    ],
+};
+
+module.exports = {
+    ParamUpdateMatrix,
+};
