ae0d0dba1e128da13a64ecdbbd6d50a30a9805ad
XuChangZJU <chinawraith@163.com>
2018-04-18 10:11:27 +0800

    提交了车辆的颜色枚举

--- src/constants/cheXiaoBao/vehicleConstant.js
+++ src/constants/cheXiaoBao/vehicleConstant.js
@@ -19,9 +19,26 @@ const decodeSpec = (t) => {
     return STRINGS[t];
 };
 
+const colors = {
+    '红': 0xFF0000,
+    '黄': 0xFFFF00,
+    '绿': 0x00FF00,
+    '蓝': 0x0000FF,
+    '黑': 0x000000,
+    '白': 0xFFFFFF,
+    '灰': 0x545454,
+    '紫': 0x9932CD,
+    '青': 0x5F9F9F,
+    '亮金': 0xD9D919,
+    '亮铜': 0xD98719,
+    '墨绿': 0x00FFFF,
+    '洋红': 0xFF00FF,
+};
+
 
 
 module.exports = {
     spec,
     decodeSpec,
+    colors,
 };
