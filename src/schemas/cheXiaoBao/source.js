8e0a5a878d018883ad94ff92798273b2a2211802
XuChangZJU <chinawraith@163.com>
2018-05-25 15:14:56 +0800

    移除了schema

--- src/schemas/cheXiaoBao/source.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2016/6/29.
- */
-'use strict';
-const assign = require('lodash/assign');
-const SysSources = require('../sysSources');
-
-module.exports = assign({}, SysSources);
--- src/schemas/sysSources.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2016/6/29.
- */
-'use strict';
-
-module.exports = {
-    mysql: 'mysql',
-};
