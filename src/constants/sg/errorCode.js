d519e7ce766d3a508f23f0f960132e4d70522645
XuChangZJU <chinawraith@163.com>
2018-06-07 17:58:53 +0800

    提交了新的errorcode

--- src/constants/sg/errorCode.js
+++ src/constants/sg/errorCode.js
@@ -5,6 +5,7 @@ const commonCode = require('../errorCode');
 
 const errorCode = {
     noUserInfo: {code: 10004, message: '您还没有告诉小高算卦需要的信息呢'},
+    uiExpired: {code: 10005, message: '您需要续费'},
 };
 
 module.exports = Object.assign(errorCode, commonCode);
