3fbf0da8b24869686d263f5e1ca8b340be290059
XuChangZJU <chinawraith@163.com>
2018-10-10 16:37:10 +0800

    moveVehicle增加了failed状态

--- src/constants/ncxb/moveVehicle.js
+++ src/constants/ncxb/moveVehicle.js
@@ -9,6 +9,7 @@ const state = {
     send: 101,
     answered: 102,
     answered2: 103,
+    failed: 201,
     end: 1001,
     end2: 1002,
 };
@@ -19,6 +20,7 @@ const decodeState = (s) => {
         [state.send]: '已请求',
         [state.answered]: '已响应',
         [state.answered2]: '已被动响应',
+        [state.failed]: '已失败',
         [state.end]: '已结束',
         [state.end2]: '已被动结束',
     };
@@ -31,6 +33,7 @@ const StateTransformMatrix = {
         [state.init]: [ state.send ],
         [state.answered]: [ state.end, state.send ],
         [state.answered2]: [ state.end, state.send ],
+        [state.failed]: [ state.send ],
     },
     [Roles.VehicleManager.name]: {
         [state.send]: [ state.answered],
@@ -38,13 +41,13 @@ const StateTransformMatrix = {
         [state.answered2]: [ state.end ],
     },
     [Roles.MoveVehicleOperator.name]: {
-        [state.send]: [ state.answered2],
+        [state.send]: [ state.answered2, state.failed ],
     },
     [Roles.ROOT.name]: {
-        [state.init]: [ state.end2],
-        [state.send]: [ state.end2],
-        [state.answered]: [ state.end2],
-        [state.answered2]: [ state.end2],
+        [state.init]: [ state.end2 ],
+        [state.send]: [ state.end2, state.failed ],
+        [state.answered]: [ state.end2 ],
+        [state.answered2]: [ state.end2 ],
     },
 };
 
