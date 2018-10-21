d6575ec0d47d23c7c8b79e43ee23e25a4c10e885
XuChangZJU <chinawraith@163.com>
2018-10-21 15:27:53 +0800

    定义了batchApplication的state

---
+++ src/constants/ncxb/batchApplication.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Administrator on 2018/10/21.
+ */
+const assert = require('assert');
+const assign = require('lodash/assign');
+const Roles = require('./role');
+const { state: CommonState } = require('../action');
+
+const state = assign({
+    // init
+    // unpaid
+    // legal
+    ready: 10001,
+    end: 10101,
+}, CommonState);
+
+const decodeState = (s) => {
+    const STRING = {
+        [state.init]: '初始',
+        [state.unpaid]: '待支付',
+        [state.legal]: '已支付',
+        [state.ready]: '生产中',
+        [state.end]: '已完成',
+        [state.expired]: '已过期',
+    };
+
+    return STRING[s];
+};
+
+const StateTransformMatrix = {
+    [Roles.LOGGEDIN]: {
+        [state.init]: [ state.unpaid ],
+    },
+    [Roles.ROOT.name]: {
+        [state.init]: [ state.expired ],
+        [state.unpaid]: [ state.legal, state.expired],
+        [state.legal]: [ state.ready ],
+        [state.ready]: [ state.end ],
+    },
+};
+
+module.exports = {
+    state,
+    decodeState,
+    StateTransformMatrix,
+};
