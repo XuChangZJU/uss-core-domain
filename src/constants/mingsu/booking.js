acc4b18bb3d959df39b88465e522b3f139c954d1
XuChangZJU <chinawraith@163.com>
2019-01-28 17:48:14 +0800

    增加了booking的category

--- src/constants/mingsu/booking.js
+++ src/constants/mingsu/booking.js
@@ -6,7 +6,23 @@ const {
     decodeState,
 } = require('../action');
 
+const category = {
+    platform: 1,
+    fromLord: 2,
+};
+
+const decodeCategory = (c) => {
+    const STRING = {
+        [category.platform]: '来自平台',
+        [category.fromLord]: '房东建立',
+    };
+
+    return STRING[c];
+}
+
 module.exports = {
     state,
     decodeState,
+    category,
+    decodeCategory,
 };
