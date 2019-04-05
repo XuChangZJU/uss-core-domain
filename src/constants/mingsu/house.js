49656dbf2128d0e9571ae6d07760564b0e4d7cd5
XuChangZJU <chinawraith@163.com>
2019-04-05 16:36:25 +0800

    提交了house常量的重定义

--- src/constants/mingsu/house.js
+++ src/constants/mingsu/house.js
@@ -2,11 +2,54 @@
  * Created by Administrator on 2019/1/28.
  */
 const {
-    category,
-    decodeCategory,
-    state,
-    decodeState,
-} = require('../ble/houseConstant');
+    state: CommonState,
+    decodeState: decodeCommonState,
+    action: CommonAction,
+    decodeAction: decodeCommonAction,
+} = require('../action');
+
+
+const category = {
+    house: 1,
+    flatShare: 2,
+    tavern: 5,
+    apartment: 13,
+    hotel: 15,
+    spec: 99,
+    room: 199,
+};
+
+
+const decodeCategory = (c) => {
+    const CATEGORY_MATRIX = {
+        [category.house]: '整租',
+        [category.flatShare]: '合租',
+        [category.tavern]: '民宿',
+        [category.apartment]: '公寓',
+        [category.hotel]: '酒店',
+        [category.spec]: '规格',
+        [category.room]: '单间',
+    };
+    return CATEGORY_MATRIX[c];
+};
+
+const state = Object.assign({
+    uncompleted: 10001,
+    online: 10011,
+    offline: 10012,
+    offlineByPlatform: 10101,
+}, CommonState);
+
+const decodeState = (s) => {
+    const STRING = {
+        [state.uncompleted]: '未完成',
+        [state.online]: '已上线',
+        [state.offline]: '已下线',
+        [state.offlineByPlatform]: '被下线'
+    };
+
+    return decodeCommonState(s) || STRING[s];
+}
 
 const membership = {
     level0: 1,
@@ -24,6 +67,26 @@ const decodeMembership = (ms) => {
     return STRING[ms];
 };
 
+const action = Object.assign({
+    bind: 1001,
+    unbind: 1002,
+    changeBind: 1003,
+    online: 1010,
+    offline: 1011,
+}, CommonAction);
+
+const decodeAction = (a) => {
+    const STRING = {
+        [action.bind]: '绑定锁',
+        [action.unbind]: '解绑锁',
+        [action.changeBind]: '换绑锁',
+        [action.online]: '上线',
+        [action.offline]: '下线',
+    };
+
+    return decodeCommonAction(a) || STRING[a];
+};
+
 module.exports = {
     category,
     decodeCategory,
@@ -31,4 +94,6 @@ module.exports = {
     decodeState,
     membership,
     decodeMembership,
+    action,
+    decodeAction,
 };
