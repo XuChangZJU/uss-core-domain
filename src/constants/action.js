6a43204fd19c65306691f55e2c5ee66c5a2dc791
Xc <thewraith@21cn.com>
2019-04-08 22:14:45 +0800

    增加了abandoned状态

--- src/constants/action.js
+++ src/constants/action.js
@@ -21,6 +21,7 @@ const action = {
     abort: 34,                  // 中断
     payExpire: 35,             // 过期
     makePaid: 36,           // 管理员确认支付
+    abandon: 37,            // 卖方主动中止
 };
 
 // 全局抽象的关系 0-1000
@@ -36,9 +37,10 @@ const state = {
     // pay相关的
     unpaid: 31,
     legal: 32,
-    aborted: 34,
-    cancelled: 35,
+    aborted: 34,            // 支付后取消
+    cancelled: 35,          // 未支付放弃
     cantPaid: 36,           // 当unpaid的支付因为某个实体的状态暂时无法支付时可以使用这个状态储存一下（比如相关产品没有库存了）
+    abandoned: 37,          // 卖方中止（pay在外部结算）
     refunding: 51,
     refunded: 52,
 
