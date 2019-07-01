bebd7e5a33ac1dbfa037cb333170427a1cf37943
Xc <thewraith@21cn.com>
2019-07-01 22:02:31 +0800

    new error code

--- src/constants/chaser/errorCode.js
+++ src/constants/chaser/errorCode.js
@@ -5,7 +5,8 @@ const CommonErrorCode = require('../errorCode');
 
 const ErrorCode = Object.assign({}, CommonErrorCode, {
     errorNeedConnectDevice: { code: 1001, message: '请靠近并连接设备'},
-    errorDeviceExisted: { code: 1002, message: '设备不存在' },
+    errorNeedDeviceName: { code: 1002, message: '请靠近设备' },
+    errorDeviceExisted: { code: 1010, message: '设备不存在' },
 });
 
 module.exports = ErrorCode;
