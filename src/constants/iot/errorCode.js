7ca00568838c6ad82fe30b6fd020042991988b18
XuChangZJU <chinawraith@163.com>
2019-07-06 10:43:17 +0800

    增加了无代理权的errorcode

--- src/constants/iot/errorCode.js
+++ src/constants/iot/errorCode.js
@@ -6,6 +6,7 @@ const CommonErrorCode = require('../errorCode');
 const ErrorCode = Object.assign({}, CommonErrorCode, {
     errorNeedConnectDevice: { code: 1001, message: '请靠近并连接设备'},
     errorNeedDeviceName: { code: 1002, message: '请靠近设备' },
+    errorNotAgency: { code: 1003, message: '您没有代理权，请联系相关人员或厂商'},
     errorDeviceUnexisted: { code: 1010, message: '设备不存在' },
     errorDeviceInconsistency: { code: 1011, message: '设备断连过久，无法使用'},
 });
