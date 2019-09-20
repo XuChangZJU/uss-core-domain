98ae428fdd7cca357745fb6c9d8153be935ff25a
XuChangZJU <chinawraith@163.com>
2019-09-20 17:47:16 +0800

    微调了operationAuth

--- src/constraints/iot/operationAuth.js
+++ src/constraints/iot/operationAuth.js
@@ -8,18 +8,20 @@ const ErrorCode = require('../../constants/iot/errorCode');
 
 const MATRIX = {
     agency: {
-        [AgencyAction.create]: {
+        [AgencyAction.update]: {
             // visible: 'always',           /*** always: 长显示，none: 不显示 *//
             auths: [
                 {
-                    '#roleNames': [Roles.EDITOR.name],
+                    '#role': [Roles.EDITOR.name],
                 },
                 {
-                    '#relation': {
+                    '#relation': {              // 表示现有对象与user的关系
+                        show: false,                           // 置true的话，前台仍然显示按钮
                         attr: 'parent',         // 这里的attr指对象（agency)中的属性，通过Schema反查找，如果没有则是指自身，即使用id属性
                         relations: [AgencyRelation.owner],         // 如果没有relations，则任何关系都可以
                     },
-                    '#attr': {
+                    '#attr': {                  // 表示对现有对象或者用户的属性
+                        show: true,                           // 置true的话，前台仍然显示按钮
                         rel: 'user',                        // 这里的rel可以是user，如果为空默认是对该行数据检查
                         check: ({ user }) => {                 // 如果是user传过来的是({ user })，如果是行数据传过来的是({ row, data })
                             if (!user.mobile) {
@@ -34,7 +36,7 @@ const MATRIX = {
             auths: [
                 {
                     '#relation': {
-                        attr: 'parentId',
+                        attr: 'parent',
                         relations: [AgencyRelation.owner],
                     },
                 }
