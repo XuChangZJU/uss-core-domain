afe59b978a432d9fea53e67403a27cf3442156f0
wjr <wenjiarui1@126.com>
2020-02-27 17:37:39 +0800

    lensconstants

---
+++ src/constants/lens/patient.js
@@ -0,0 +1,43 @@
+
+const {
+    action: commonAction,
+    decodeAction: decodeCommonAction,
+    relation: commonRelation,
+    decodeRelation: decodeCommonRelation,
+} = require('../action');
+
+
+const relation = Object.assign({}, commonRelation, {
+    self: 101,
+    father: 111,
+    mother: 112,
+    eldership: 113,
+    sibling: 121,
+    friend: 122,
+    colleague: 123,
+    spouse: 124,
+});
+
+
+const decodeRelation = (r) => {
+    const S = {
+        [relation.self]: '自身',
+        [relation.father]: '父亲',
+        [relation.mother]: '母亲',
+        [relation.eldership]: '长辈',
+        [relation.sibling]: '兄弟姐妹',
+        [relation.friend]: '朋友',
+        [relation.colleague]: '同事',
+        [relation.spouse]: '配偶',
+    };
+
+    return S[r] || decodeCommonRelation(r);
+};
+
+
+module.exports = {
+    relation,
+    decodeRelation,
+    action,
+    decodeAction,
+};
--- src/constants/lens/record.js
+++ src/constants/lens/record.js
@@ -1,40 +1,37 @@
-/**
- * Created by Xc on 2020/2/20.
- */
 const { action: commonAction, decodeAction: decodeCommonAction } = require('../action');
 const state = {
-    active: 301,
-    completed: 310,
+    unbinded: 301,
+    binded: 310,
     expired: 511,
 };
 
 const decodeState = (s) => {
     const S = {
-        [state.active]: '诊疗中',
-        [state.completed]: '已完成',
+        [state.unbinded]: '未绑定',
+        [state.binded]: '已绑定',
         [state.expired]: '已过期',
     };
 
     return S[s];
 };
 
-const action = Object.assign({},commonAction,{
-    complete: 310,
+const action = Object.assign({}, commonAction ,{
+    bind: 310,
     expire: 511,
 });
 
 const decodeAction = (a) => {
     const S = {
-        [action.complete]: '完成',
-        [action.expire]: '结束',
+        [action.bind]: '绑定',
+        [action.expire]: '过期',
     };
 
-    return S[a]|| decodeCommonAction(a);
+    return S[a]||decodeCommonAction(a);
 };
 
 const STATE_TRANS_MATRIX = {
-    [action.complete]: [state.active, state.completed],
-    [action.expire]: [state.active, state.expired],
+    [action.bind]: [state.unbinded, state.binded],
+    [action.expire]: [state.unbinded, state.expired],
 };
 
 module.exports = {
