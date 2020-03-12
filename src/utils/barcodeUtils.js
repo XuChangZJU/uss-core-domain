5ecbcc3a9f16c59ab9803b854d8d21df3a0abbd3
Xc <thewraith@21cn.com>
2020-03-12 21:05:37 +0800

    barcodeUtils

---
+++ src/utils/barcodeUtils.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Xc on 2020/3/12.
+ */
+function encodeBarcode(entity, uuid) {
+    return `${entity}-${uuid}`;
+}
+
+function decodeBarcode(barcode) {
+    return barcode.split('-');
+}
+
+module.exports = {
+    encodeBarcode,
+    decodeBarcode,
+};
