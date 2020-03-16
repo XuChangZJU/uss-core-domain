c3e6ac82c3d16ef753e6080fc1083bdd9877ebec
Xc <thewraith@21cn.com>
2020-03-16 21:47:36 +0800

    增加了lens中的BUSINESS角色

--- src/constants/lens/roles.js
+++ src/constants/lens/roles.js
@@ -1,4 +1,15 @@
 /**
  * Created by Xc on 2020/2/20.
  */
+const { Roles: CommonRoles } = require('../roleConstant2');
 
+const Roles = Object.assign({}, CommonRoles, {
+    BUSINESS: {
+        name: 'business',
+        id: 101,
+    },
+});
+
+module.exports = {
+    Roles,
+};
