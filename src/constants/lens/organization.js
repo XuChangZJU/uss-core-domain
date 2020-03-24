e3621c1f27ad4126aee06e343eb8547184a5c217
邢龙 <2435409107@qq.com>
2020-03-24 15:28:08 +0800

    organization 添加bind，unbind动作

--- src/constants/lens/organization.js
+++ src/constants/lens/organization.js
@@ -4,12 +4,30 @@ const {
 } = require('../action');
 
 const {
-    action,
-    decodeAction,
+    action: commonAction,
+    decodeAction: decodeCommonAction,
     state,
     decodeState,
-    STATE_TRANS_MATRIX,
+    STATE_TRANS_MATRIX: COMMON_STATE_TRANS_MATRIX,
 } = require('./common');
+
+const action = Object.assign({}, commonAction, {
+    bind: 1001,
+    unbind: 1002,
+});
+
+const decodeAction = (a) => {
+    const TEXT = {
+        [action.bind]: '绑定',
+        [action.unbind]: '解绑',
+    };
+    return TEXT[a] || decodeCommonAction(a);
+};
+
+const STATE_TRANS_MATRIX = Object.assign({}, COMMON_STATE_TRANS_MATRIX, {
+    [action.bind]: [state.init, state.online],
+    [action.unbind]: [state.online, state.offline],
+});
 
 module.exports = {
     relation,
