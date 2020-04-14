d591561f0794a8fb3cb5e918a77a52796f7acee8
wjr <wenjiarui1@126.com>
2020-04-14 16:50:03 +0800

    fix

--- src/constraints/lens/businessDef.js
+++ src/constraints/lens/businessDef.js
@@ -688,30 +688,33 @@ const AUTH_MATRIX = {
                         },
                     ],
                 },
-                // {
-                //     '#exists': [
-                //         {
-                //             relation: 'userWorker',
-                //             needData: true,
-                //             condition: ({user, row, actionData}) => {
-                //                 const { worker } = actionData;
-                //                 const { organizationId, jobId, id } = row;
-                //                 const { number, jobId: jobId2 } = worker;
-                //                 if((number && !/^[0-9a-zA-Z_-]+$/.test(number)))
-                //                     throw new Error('请填写正确的工号');
-                //                 if(!jobId2 ) {
-                //                     return {
-                //                         userId: user.id,
-                //                         worker: {
-                //                             organizationId,
-                //                         },
-                //                         workerId: id,
-                //                     }
-                //                 }
-                //             }
-                //         }
-                //     ],
-                // },
+                {
+                    '#exists': [
+                        {
+                            relation: 'userWorker',
+                            needData: true,
+                            condition: ({user, row, actionData}) => {
+                                const { worker } = actionData;
+                                const { organizationId, jobId, id } = row;
+                                const { number, jobId: jobId2 } = worker;
+                                if((number && !/^[0-9a-zA-Z_-]+$/.test(number)))
+                                    throw new Error('请填写正确的工号');
+                                if(!jobId2 ) {
+                                    return {
+                                        userId: user.id,
+                                        worker: {
+                                            organizationId,
+                                            id
+                                        },
+                                    }
+                                }
+                                return{
+                                    userId: -1,
+                                }
+                            }
+                        }
+                    ],
+                },
             ],
         },
         [WorkerAction.remove]: {
