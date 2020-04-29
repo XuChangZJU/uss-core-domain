6bd912c408cc071627125d3a68fbbb70d78ba32e
wjr <wenjiarui1@126.com>
2020-04-29 12:23:36 +0800

    'add'

---
+++ src/constants/lens/diagnosisResult.js
@@ -0,0 +1,10 @@
+const { action: commonAction, decodeAction: decodeCommonAction } = require('../action');
+
+const action = commonAction;
+
+const decodeAction = decodeCommonAction;
+
+module.exports = {
+    action,
+    decodeAction,
+};
