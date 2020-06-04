0ee6478566cc74e53e502231c02b3b8ce45bfe28
wjr <wenjiarui1@126.com>
2020-06-04 17:42:04 +0800

    '结算相关对象的constants'

---
+++ src/constants/vendue/checkOut.js
@@ -0,0 +1,19 @@
+const {
+    action,
+    decodeAction,
+    state,
+    decodeState,
+    relation,
+    decodeRelation,
+    COMMON_STATE_TRAN_MATRIX,
+} = require('../action');
+
+module.exports = {
+    action,
+    decodeAction,
+    state,
+    decodeState,
+    relation,
+    decodeRelation,
+    COMMON_STATE_TRAN_MATRIX,
+};
