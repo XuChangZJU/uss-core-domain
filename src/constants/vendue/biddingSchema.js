743759804b40a8bde1efdc4d06501c6df9f87897
wjr <wenjiarui1@126.com>
2020-06-08 14:34:02 +0800

    'biddingschematype'

---
+++ src/constants/vendue/biddingSchema.js
@@ -0,0 +1,9 @@
+const type = Object.assign({}, {
+    sequentiallyIncreasing: 1,
+    increasingBy258: 2,
+});
+
+const STRINGS_OF_ORIGINS = {
+    [type.sequentiallyIncreasing]: "顺序递增",
+    [type.increasingBy258]: "258拍",
+};
