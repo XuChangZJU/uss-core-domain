aec834b084a7808b1e7531dcdddfb9d50a06d392
wjr <wenjiarui1@126.com>
2020-06-08 15:32:50 +0800

    'fix'

--- src/constants/vendue/biddingSchema.js
+++ src/constants/vendue/biddingSchema.js
@@ -7,3 +7,13 @@ const STRINGS_OF_ORIGINS = {
     [type.sequentiallyIncreasing]: "顺序递增",
     [type.increasingBy258]: "258拍",
 };
+
+function decodeType(o) {
+    return STRINGS_OF_ORIGINS[o];
+}
+
+module.exports = {
+    type,
+    decodeType,
+};
+
