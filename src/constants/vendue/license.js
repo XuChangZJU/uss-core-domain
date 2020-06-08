f7155ad03757ca5bb6d56fdb28163595bcfad3be
wjr <wenjiarui1@126.com>
2020-06-08 17:33:20 +0800

    '增加license佣金优惠类型'

--- src/constants/vendue/license.js
+++ src/constants/vendue/license.js
@@ -1,9 +1,11 @@
 const type = Object.assign({}, {
     noInsurance: 1,
+    rewardBenifit: 2,
 });
 
 const STRINGS_OF_ORIGINS = {
     [type.noInsurance]: "免保",
+    [type.rewardBenifit]: "佣金优惠",
 };
 
 function decodeType(o) {
--- src/constants/vendue/paddle.js
+++ src/constants/vendue/paddle.js
@@ -11,12 +11,14 @@ const {
 const state = Object.assign({}, commonState, {
     unsettled: 501,
     settled: 511,
+    breakUp: 601,
 });
 
 const decodeState = (s) => {
     const S = {
         [state.unsettled]: '未结算',
         [state.settled]: '已结算',
+        [state.breakUp]: '违约',
     };
 
     return S[s] || decodeCommonState(s);
