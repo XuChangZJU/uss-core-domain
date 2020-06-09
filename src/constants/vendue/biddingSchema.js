6bf4f7c6e821a28d4bfa08d634548b18dc041707
wjr <wenjiarui1@126.com>
2020-06-09 10:37:15 +0800

    'getChangedPrice'

--- src/constants/vendue/biddingSchema.js
+++ src/constants/vendue/biddingSchema.js
@@ -1,3 +1,5 @@
+const ErrorCode = require('../errorCode');
+
 const type = Object.assign({}, {
     sequentiallyIncreasing: 1,
     increasingBy258: 2,
@@ -12,8 +14,39 @@ function decodeType(o) {
     return STRINGS_OF_ORIGINS[o];
 }
 
+
+function getChangedPrice(params){
+    const FN = {
+        [type.increasingBy258]: ({myPrice, add}) => {
+            const secondPos = [-2, 0, 2, 5, 8, 10];
+            const myPrice2 = Math.floor(myPrice);
+            let myPrice1 = myPrice2.toString();
+            if(/^10{2,}$/.test(myPrice1)){
+                let ans = '98';
+                for (let i = 0; i < myPrice1.length -3; i ++){
+                    ans += '0';
+                }
+                return parseInt(ans);
+            }
+            if (myPrice1.length < 2) {
+                throw ErrorCode.createErrorByCode(ErrorCode.errorLegalParamError, '258加价格至少为10');
+            }
+            let posJudge = Math.floor((parseInt(myPrice1[1])-1)/3)+1;
+            let basic = 1;
+            for (let i = 0; i < myPrice1.length-2; i++){
+                basic *= 10;
+            }
+            return (parseInt(myPrice1[0])*10 + secondPos[posJudge+add])*basic;
+        },
+        [type.sequentiallyIncreasing]: ({myPrice, step, add}) => {
+            return myPrice + step * add;
+        }
+    }
+    return FN[params.type](params);
+}
 module.exports = {
     type,
     decodeType,
+    getChangedPrice,
 };
 
