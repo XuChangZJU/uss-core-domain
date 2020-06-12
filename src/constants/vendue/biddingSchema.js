5bb9a43bebde5a53412f6e169f674ecbd096bb3e
278599135 <278599135@qq.com>
2020-06-12 14:28:41 +0800

    竞价阶梯 计算

--- src/constants/vendue/biddingSchema.js
+++ src/constants/vendue/biddingSchema.js
@@ -14,7 +14,7 @@ function decodeType(o) {
     return STRINGS_OF_ORIGINS[o];
 }
 
-function getBS({ price, biddingSchema }){
+function getBS(price, biddingSchema){
     let b;
     for (let i = 0; i < biddingSchema.length; i++){
         if( biddingSchema[i].min<= price && biddingSchema[i].max > price){
@@ -27,18 +27,18 @@ function getBS({ price, biddingSchema }){
 
 function getChangedPrice(params){
     const FN = {
-        [type.increasingBy258]: ({myPrice, add}) => {
+        [type.increasingBy258]: ({ price, add }) => {
             const secondPos = [-2, 0, 2, 5, 8, 10];
-            const myPrice2 = Math.floor(myPrice);
+            const myPrice2 = Math.floor(price);
             let myPrice1 = myPrice2.toString();
-            if(/^10{2,}$/.test(myPrice)){
+            if(/^10{2,}$/.test(price)){
                 let ans = '98';
                 for (let i = 0; i < myPrice1.length -3; i ++){
                     ans += '0';
                 }
                 return parseInt(ans);
             }
-            if ((myPrice1.length < 2) || (myPrice === 10 && add === -1)) {
+            if ((myPrice1.length < 2) || (price === 10 && add === -1)) {
                 return 10;
             }
             let posJudge = Math.floor((parseInt(myPrice1[1])-2)/3)+2;
@@ -47,20 +47,34 @@ function getChangedPrice(params){
                 posJudge++;
             }
             let basic = 1;
-            for (let i = 0; i < myPrice1.length-2; i++){
+            for (let i = 0; i < myPrice1.length - 2; i++){
                 basic *= 10;
             }
-            return (parseInt(myPrice1[0])*10 + secondPos[posJudge+add])*basic;
+            return (parseInt(myPrice1[0]) * 10 + secondPos[posJudge + add]) * basic;
         },
-        [type.sequentiallyIncreasing]: ({myPrice, step, add}) => {
-            if(myPrice + step * add < 0){
+        [type.sequentiallyIncreasing]: ({ price, section, add }) => {
+            const { step } = section;
+            if(price + step * add < 0){
                 return 0;
             }
-            return myPrice + step * add;
+            return price + step * add;
         }
+    };
+
+    //传入 价格 竞价阶梯
+    const { price, biddingSchema } = params;
+
+    const section = getBS(price, biddingSchema); //价格在竞价阶梯哪一段
+    if (!section) {
+        return -1;
     }
-    return FN[params.type](params);
+    const fn = FN[section.type];
+    if (typeof fn !== 'function') {
+        return -1;
+    }
+    return fn(Object.assign({}, params, { section }));
 }
+
 module.exports = {
     type,
     decodeType,
