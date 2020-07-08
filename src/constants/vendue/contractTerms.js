a6afc0e77bb4b32b65db8eae25f64f6b39c2b51c
wjr <wenjiarui1@126.com>
2020-07-08 19:15:16 +0800

    'fix'

--- src/constants/vendue/contractTerms.js
+++ src/constants/vendue/contractTerms.js
@@ -10,19 +10,19 @@ const {
  * 合同状态：legal生效的、competed完成的、aborted中止的
  * 合同动作：complete完成，abort中止
  */
-const state = Object.assign({}, commonState, {
-    unsold: 301,
-    sold: 311,
-    settled: 401
-});
-const decodeState = (s) => {
-    const S = {
-        [state.unsold]: '未成交',
-        [state.sold]: '成交',
-        [state.settled]: '已结算',
-    };
-    return S[s] || decodeCommonState(s);
-};
+// const state = Object.assign({}, commonState, {
+//     unsold: 301,
+//     sold: 311,
+//     settled: 401
+// });
+// const decodeState = (s) => {
+//     const S = {
+//         [state.unsold]: '未成交',
+//         [state.sold]: '成交',
+//         [state.settled]: '已结算',
+//     };
+//     return S[s] || decodeCommonState(s);
+// };
 module.exports = {
     state,
     decodeState,
