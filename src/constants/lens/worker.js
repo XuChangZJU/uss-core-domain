30ea993445aada8f2cacf88bb90714c42da3c180
wjr <wenjiarui1@126.com>
2020-07-27 18:16:29 +0800

    'fix'

--- src/constants/lens/worker.js
+++ src/constants/lens/worker.js
@@ -1,45 +1,13 @@
 const {
-    action: commonAction,
+    action: CommonAction,
     decodeAction: decodeCommonAction,
-    relation,
-    decodeRelation,
+    relation: CommonRelation,
+    decodeRelation: decodeCommonRelation,
 } = require('../action');
 
-const state = {
-    active: 301,
-    inactive: 401
-};
-
-const decodeState = (s) => {
-    const S = {
-        [state.active]: '生效中',
-        [state.inactive]: '已完成',
-    };
-
-    return S[s];
-};
-
-const action = Object.assign({},commonAction,{
-    complete: 310,
-});
-
-const decodeAction = (a) => {
-    const S = {
-        [action.complete]: '完成',
-    };
-
-    return S[a]|| decodeCommonAction(a);
-};
-
-const STATE_TRANS_MATRIX = {
-    [action.complete]: [state.active, state.inactive],
-};
-
 module.exports = {
-    relation,
-    decodeRelation,
-    state,
-    decodeState,
-    action,
-    decodeAction,
+    relation: CommonRelation,
+    decodeRelation: decodeCommonRelation,
+    action: CommonAction,
+    decodeAction: decodeCommonAction,
 };
