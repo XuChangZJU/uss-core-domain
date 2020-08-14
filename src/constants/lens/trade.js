bb26e16b4564c8d4b939ff3dfeb0183265f38e51
wjr <wenjiarui1@126.com>
2020-08-14 11:45:43 +0800

    'fix'

--- src/constants/lens/trade.js
+++ src/constants/lens/trade.js
@@ -78,9 +78,9 @@ const decodeAction = (a) => {
 };
 
 const STATE_TRAN_MATRIX = {
-    [action.confirmArriveAtShop]: [[transportState.dfl, transportState.dzdjh, transportState.dqhcl], state.dqj],
+    [action.confirmArriveAtShop]: [[transportState.dfl, transportState.dzdjh, transportState.dqhcl], transportState.dqj],
     [action.confirmGet]: [transportState.dqj, transportState.yqj],
-    [action.send]: [[transportState.dfl, transportState.dzdjh, transportState.dqhcl], state.dqj],
+    [action.send]: [[transportState.dfl, transportState.dzdjh, transportState.dqhcl], transportState.dqj],
 };
 module.exports = {
     action,
