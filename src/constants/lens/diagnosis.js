f961a151838090044334cd4b3725296dbf29332b
wjr <wenjiarui1@126.com>
2020-08-17 18:46:49 +0800

    'diagnosiscategory'

--- src/constants/lens/diagnosis.js
+++ src/constants/lens/diagnosis.js
@@ -7,7 +7,29 @@ const state = {
     hasTrade: 310,
     expired: 511,
 };
-
+const category = {
+    '配镜': 1,
+    '角膜塑形镜': 2,
+    '成镜': 3,
+    '耗品': 4,
+    '视训': 5,
+    '配镜复查': 6,
+    '角膜塑形镜复查': 7,
+    '视训复查': 8,
+}
+const decodeCategory = (c) => {
+    const C = {
+        [category.成镜]: 1,
+        [category.角膜塑形镜]: 2,
+        [category.成镜]: 3,
+        [category.耗品]: 4,
+        [category.视训]: 5,
+        [category.配镜复查]: 6,
+        [category.角膜塑形镜复查]: 7,
+        [category.视训复查]: 8,
+    }
+    return C[c];
+}
 const decodeState = (s) => {
     const S = {
         [state.noTrade]: '无交易',
@@ -37,6 +59,8 @@ const STATE_TRANS_MATRIX = {
 module.exports = {
     action,
     decodeAction,
+    category,
+    decodeCategory,
     state,
     decodeState,
     STATE_TRANS_MATRIX,
