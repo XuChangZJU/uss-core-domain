03456e64c48e8fdb3ce6c42d2daf6f1375c82e8d
wjr <wenjiarui1@126.com>
2020-08-18 13:38:53 +0800

    'recheck'

--- src/constants/lens/recheck.js
+++ src/constants/lens/recheck.js
@@ -7,7 +7,8 @@ const state = {
     active: 2002,            // 推送中
     confirmed: 2003,         // 用户已确认
     completed: 2004,         // 复查完成
-    expired: 100001,          // 过期
+    expired: 100001,        // 过期
+    killed: 100010,         // 用户已回绝
 };
 
 const decodeState = (s) => {
@@ -16,7 +17,8 @@ const decodeState = (s) => {
         [state.active]: '已到期',
         [state.confirmed]: '已确认',
         [state.completed]: '已完成',
-        [state.expired]: '已过期'
+        [state.expired]: '已过期',
+        [state.killed]: '已回绝'
     };
 
     return S[s];
@@ -27,6 +29,7 @@ const action = {
     confirm: 2003,
     complete: 2004,
     expire: 2005,
+    kill: 2006,
 };
 
 const decodeAction = (a) => {
@@ -35,6 +38,7 @@ const decodeAction = (a) => {
         [action.confirm]: '确认',
         [action.complete]: '完成',
         [action.expire]: '过期',
+        [action.kill]: '回绝',
     };
 
     return S[a];
@@ -43,8 +47,9 @@ const decodeAction = (a) => {
 const STATE_TRANS_MATRIX = {
     [action.activate]: [state.inactive, state.active],
     [action.confirm]: [state.active, state.confirmed],
-    [action.complete]: [[state.inactive, state.active, state.confirmed, state.expired], state.confirmed],
+    [action.complete]: [[state.inactive, state.active, state.confirmed, state.expired, state.killed], state.confirmed],
     [action.expire]: [[state.active, state.confirmed], state.expired],
+    [action.kill]: [state.expired, state.killed]
 };
 
 module.exports = {
