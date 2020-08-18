03f4c833885827207ded1b483f29cd04f22440b9
wjr <wenjiarui1@126.com>
2020-08-18 20:21:41 +0800

    'tradeaction权限修改'

--- src/constraints/lens/businessDef.js
+++ src/constraints/lens/businessDef.js
@@ -580,7 +580,7 @@ const AUTH_MATRIX = {
                     '#data': [                 // 表示对现有对象或者用户的数据有要求，可以有多项，每项之间是AND的关系
                         {
                             check: ({user, row}) => {
-                                return [TradeTransportState.dfl, TradeTransportState.dzdjh, TradeTransportState.dqhcl, TradeTransportState.dqj].includes(row.transportState) && row.getMethod === TradeGetMethod.helpYourself;
+                                return [TradeTransportState.dqj].includes(row.transportState) && row.getMethod === TradeGetMethod.helpYourself;
                             },
                         }
                     ],
@@ -592,7 +592,7 @@ const AUTH_MATRIX = {
                     '#data': [                 // 表示对现有对象或者用户的数据有要求，可以有多项，每项之间是AND的关系
                         {
                             check: ({user, row}) => {
-                                return [TradeTransportState.dfl, TradeTransportState.dzdjh, TradeTransportState.dqhcl, TradeTransportState.dqj].includes(row.transportState) && row.getMethod === TradeGetMethod.helpYourself;
+                                return [TradeTransportState.dqj].includes(row.transportState) && row.getMethod === TradeGetMethod.helpYourself;
                             },
                         }
                     ],
