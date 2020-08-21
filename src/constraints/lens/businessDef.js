4c54d91c100bf3a145e9f83c56c5f8a90a90ac31
wjr <wenjiarui1@126.com>
2020-08-21 11:35:35 +0800

    'fixarriveatshop'

--- src/constraints/lens/businessDef.js
+++ src/constraints/lens/businessDef.js
@@ -498,7 +498,7 @@ const AUTH_MATRIX = {
                     '#data': [                 // 表示对现有对象或者用户的数据有要求，可以有多项，每项之间是AND的关系
                         {
                             check: ({user, row}) => {
-                                return [TradeTransportState.dqhcl, TradeTransportState.dzdjh, TradeTransportState.dfl].includes(row.transportState) && row.getMethod !== TradeGetMethod.helpYourself;
+                                return [TradeTransportState.dqhcl, TradeTransportState.dzdjh, TradeTransportState.dfl].includes(row.transportState) && row.getMethod === TradeGetMethod.helpYourself;
                             },
                         }
                     ],
