2f7d4dbd9c38681fb10f97f12f1b1643a2ce44bb
wjr <wenjiarui1@126.com>
2020-08-24 17:40:47 +0800

    'fix'

--- src/constraints/lens/businessDef.js
+++ src/constraints/lens/businessDef.js
@@ -1420,21 +1420,21 @@ const AUTH_MATRIX = {
                 ],
             }]
         },
-        [OrganizationAction.eternaliseQrCode]: {
-            auths: [
-                {
-                    "#relation": {
-                        relations: [OrganizationRelation.owner],
-                    },
-                },
-                {
-                    "#relation": {
-                        attr: 'brand',
-                        relations: [BrandRelation.owner],
-                    },
-                }
-            ]
-        },
+        // [OrganizationAction.eternaliseQrCode]: {
+        //     auths: [
+        //         {
+        //             "#relation": {
+        //                 relations: [OrganizationRelation.owner],
+        //             },
+        //         },
+        //         {
+        //             "#relation": {
+        //                 attr: 'brand',
+        //                 relations: [BrandRelation.owner],
+        //             },
+        //         }
+        //     ]
+        // },
         // [OrganizationAction.transfer]:{
         //     auths: [
         //         {
