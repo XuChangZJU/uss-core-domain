a3fac8e4061a4779bf91a03e20f6d9089d461012
wjr <wenjiarui1@126.com>
2020-08-31 16:57:10 +0800

    'brand增加销售人员'

--- src/constants/lens/brand.js
+++ src/constants/lens/brand.js
@@ -8,13 +8,15 @@ const {
 const relation = Object.assign({}, commonRelation, {
     worker: 301,
     customerService: 401,
+    seller: 501
 });
 
 
 const decodeRelation = (r) => {
     const S = {
         [relation.worker]: '工作人员',
-        [relation.customerService]: '运营人员'
+        [relation.customerService]: '运营人员',
+        [relation.seller]: '销售人员',
     };
 
     return S[r] || decodeCommonRelation(r);
