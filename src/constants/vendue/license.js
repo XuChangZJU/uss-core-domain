51d1e368a6b8b5027fd8f0c5adb2d470dfc91c80
wjr <wenjiarui1@126.com>
2020-09-21 18:12:33 +0800

    'fix'

--- src/constants/vendue/license.js
+++ src/constants/vendue/license.js
@@ -10,11 +10,13 @@ const {
 const type = Object.assign({}, {
     noInsurance: 1,
     rewardBenifit: 2,
+    depositBenifit: 3,
 });
 
 const STRINGS_OF_ORIGINS = {
     [type.noInsurance]: "免保",
     [type.rewardBenifit]: "佣金优惠",
+    [type.depositBenifit]: "保证金优惠",
 };
 
 function decodeType(o) {
