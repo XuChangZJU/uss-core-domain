53dfbe684aba68fa9e711196f8f86a7646a56fa6
wjr <wenjiarui1@126.com>
2020-09-23 19:40:07 +0800

    'fix'

--- src/constraints/lens/businessDef.js
+++ src/constraints/lens/businessDef.js
@@ -1047,9 +1047,7 @@ const AUTH_MATRIX = {
                                 const query = {
                                     userId: user.id,
                                     relation: {
-                                        relation: {
-                                            $in: [BrandRelation.owner, BrandRelation.customerService, BrandRelation.manager, BrandRelation.seller],
-                                        },
+                                        $in: [BrandRelation.owner, BrandRelation.customerService, BrandRelation.manager, BrandRelation.seller],
                                     }
                                 };
                                 return query;
