96876b2c77d546d09acebaf3b598428c1dcf43bd
Gavin6581 <61622958+Gavin6581@users.noreply.github.com>
2020-09-27 11:13:00 +0800

    Update memberAchievement.js
    添加了assign的定义

--- src/constants/xhs/memberAchievement.js
+++ src/constants/xhs/memberAchievement.js
@@ -2,6 +2,7 @@
  * Created by Xc on 2020/8/20.
  */
 const pick = require('lodash/pick');
+const assign = require('lodash/assign');
 const { action: CommonAction, decodeAction: decodeCommonAction } = require('../action');
 
 const action = assign(
