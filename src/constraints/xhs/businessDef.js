9cc8c14dbacfc2e512bafa4dbe2aa2873c50454e
Gavin6581 <3122655867@qq.com>
2020-10-06 11:17:12 +0800

    更新了project的权限操作

--- src/constraints/xhs/businessDef.js
+++ src/constraints/xhs/businessDef.js
@@ -16,18 +16,34 @@ const {
 } = require('../../constants/xhs/memberAchievement');
 
 const {
+    action: ProjectAction,
+} = require('../../constants/xhs/project');
+
+const {
+    action: MemberProjectAction,
+} = require('../../constants/xhs/memberProject');
+
+const {
+    action: StarAction,
+} = require('../../constants/xhs/star');
+
+const {
+    action: CommentAction,
+} = require('../../constants/xhs/comment');
+
+const {
     AllowEveryoneAuth,
     OwnerRelationAuth,
     AnyRelationAuth,
-    } = require('../action');
+} = require('../action');
 
-const { Roles } = require('../../constants/lens/roles');
+const {Roles} = require('../../constants/lens/roles');
 
 const MemberOfAchievement = {
     '#exists': [
         {
             relation: 'memberAchievement',
-            condition: ({ user, row }) => {
+            condition: ({user, row}) => {
                 const query = {
                     member: {
                         userId: user.id,
@@ -44,13 +60,47 @@ const MemberAchievementOption = {
     '#exists': [
         {
             relation: 'memberAchievement',
-            condition: ({ user, row }) => {
+            condition: ({user, row}) => {
                 const query = {
                     member: {
                         userId: user.id,
                     },
                     // achievementId: row.id,
                     memberAchievementId: row.id,
+                };
+                return query;
+            },
+        },
+    ],
+};
+
+const MemberOfProject = {
+    '#exists': [
+        {
+            relation: 'memberProject',
+            condition: ({user, row}) => {
+                const query = {
+                    member: {
+                        userId: user.id,
+                    },
+                    projectId: row.id,
+                };
+                return query;
+            },
+        },
+    ],
+};
+
+const MemberProjectOption = {
+    '#exists': [
+        {
+            relation: 'memberProject',
+            condition: ({user, row}) => {
+                const query = {
+                    member: {
+                        userId: user.id,
+                    },
+                    memberProjectId: row.id,
                 };
                 return query;
             },
@@ -81,7 +131,7 @@ const AUTH_MATRIX = {
                     '#exists': [
                         {
                             relation: 'member',
-                            condition: ({ user }) => {
+                            condition: ({user}) => {
                                 const query = {
                                     userId: user.id,
                                     role: {
@@ -113,7 +163,7 @@ const AUTH_MATRIX = {
                     '#exists': [
                         {
                             relation: 'member',
-                            condition: ({ user }) => {
+                            condition: ({user}) => {
                                 const query = {
                                     member: {
                                         userId: user.id,
@@ -135,6 +185,168 @@ const AUTH_MATRIX = {
             auths: [
                 MemberAchievementOption,
             ],
+        },
+        [MemberAchievementAction.exchange]: {
+            auths: [
+                {
+                    '#exists': [
+                        {
+                            relation: 'memberAchievement',
+                            condition: ({user, row}) => {
+                                const query = {
+                                    member: {
+                                        userId: user.id,
+                                    },
+                                    memberAchievementId: row.id,
+                                };
+                                return query;
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+    },
+    project: {
+        [ProjectAction.create]: {
+            auths: [
+                {
+                    '#exists': [
+                        {
+                            relation: 'member',
+                            condition: ({user}) => {
+                                const query = {
+                                    userId: user.id,
+                                    role: {
+                                        $in: [MemberRole.teacher, MemberRole.graduate,MemberRole.student],
+                                    }
+                                };
+                                return query;
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+        [ProjectAction.update]: {
+            auths: [
+                MemberOfProject,
+            ],
+        },
+        [ProjectAction.remove]: {
+            auths: [
+                MemberOfProject,
+            ],
+        },
+        [ProjectAction.try]: {
+            auths: [
+                MemberOfProject,
+            ],
+        },
+        [ProjectAction.share]: {
+            auths: [
+                MemberOfProject,
+            ],
+        },
+    },
+    memberProject: {
+        [MemberProjectAction.create]: {
+            auths: [
+                {
+                    '#exists': [
+                        {
+                            relation: 'member',
+                            condition: ({user}) => {
+                                const query = {
+                                    member: {
+                                        userId: user.id,
+                                    },
+                                };
+                                return query;
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+        [MemberProjectAction.update]: {
+            auths: [
+                MemberProjectOption,
+            ],
+        },
+        [MemberProjectAction.remove]: {
+            auths: [
+                MemberProjectOption,
+            ],
+        },
+        [MemberProjectAction.exchange]: {
+            auths: [
+                {
+                    '#exists': [
+                        {
+                            relation: 'memberProject',
+                            condition: ({user, row}) => {
+                                const query = {
+                                    member: {
+                                        userId: user.id,
+                                    },
+                                    memberProjectId: row.id,
+                                };
+                                return query;
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+    },
+    star: {
+        [StarAction.update]: {
+            auths: [
+                {
+                    '#data': [
+                        {
+                            check: ({user, row}) => {
+                                return user.id;
+                            },
+                        }
+                    ],
+                }
+            ]
+        },
+    },
+    comment: {
+        [CommentAction.create]: {
+            auths: [
+                {
+                    '#exists': [
+                        {
+                            relation: 'member',
+                            condition: ({user}) => {
+                                const query = {
+                                    member: {
+                                        userId: user.id,
+                                    },
+                                };
+                                return query;
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
+        [CommentAction.update]: {
+            auths: [
+                {
+                    '#data': [
+                        {
+                            check: ({user, row}) => {
+                                return user.id;
+                            },
+                        }
+                    ],
+                }
+            ]
         },
     },
 };
