5964a7aab7ce5347b4a9c10643eb739154f698c9
wjr <wenjiarui1@126.com>
2020-10-26 17:41:00 +0800

    'fxi'

--- src/constants/lens/checkIn.js
+++ src/constants/lens/checkIn.js
@@ -22,13 +22,15 @@ const state = {
     add: 2,
     late: 3,
     earlyLeave: 4,
+    different: 8
 }
 const decodeState = (s) => {
     const S = {
         [state.normal]: '正常打卡',
         [state.add]: '补卡',
         [state.late]: '迟到',
-        [state.earlyLeave]: '早退'
+        [state.earlyLeave]: '早退',
+        [state.different]: '打卡门店与排班门店不同'
     }
     return S[s];
 }
