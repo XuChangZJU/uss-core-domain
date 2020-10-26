2420e08dcb5f9a677711a78acd7762129ee71ef1
wjr <wenjiarui1@126.com>
2020-10-26 10:03:48 +0800

    'fix'

--- src/constraints/lens/businessDef.js
+++ src/constraints/lens/businessDef.js
@@ -125,7 +125,7 @@ const AUTH_MATRIX = {
                     '#data': [                 // 表示对现有对象或者用户的数据有要求，可以有多项，每项之间是AND的关系
                         {
                             check: ({user, row}) => {
-                                return [TradeTransportState.wdd, TradeTransportState.yqj].includes(row.transportState);
+                                return [TradeTransportState.wdd, TradeTransportState.yqj].includes(row.transportState) && TradeState.legal2 === row.state;
                             },
                         }
                     ],
