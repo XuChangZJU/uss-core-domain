938f632e9776125ea5f7be460e2c19b17ca4cbba
Gavin6581 <3122655867@qq.com>
2020-11-13 18:45:32 +0800

    添加了member的color

--- src/constants/xhs/member.js
+++ src/constants/xhs/member.js
@@ -8,19 +8,28 @@ const role = {
     teacher: 1,
     graduate: 2,
     student: 3,
-    colorA: 1,
-    colorB: 2,
-    colorC: 3,
 };
 
+const roleColor = {
+    colorTeacher: 10,
+    colorGraduate: 11,
+    colorStudent: 12,
+};
 const decodeRole = (r) => {
     const T = {
         [role.teacher]: '教师',
         [role.graduate]: '研究生',
         [role.student]: '学生',
-        [role.colorA]: '#FA7474',
-        [role.colorB]: '#396A9F',
-        [role.colorC]: '#68A0DD',
+    };
+
+    return T[r];
+};
+
+const decodeRoleColor = (r) => {
+    const T = {
+        [roleColor.colorTeacher]: '#FA7474',
+        [roleColor.colorGraduate]: '#396A9F',
+        [roleColor.colorStudent]: '#68A0DD',
     };
 
     return T[r];
@@ -30,7 +39,9 @@ const action = pick(CommonAction, ['update']);
 
 module.exports = {
     role,
+    roleColor,
     decodeRole,
+    decodeRoleColor,
     action,
     decodeAction,
 };
