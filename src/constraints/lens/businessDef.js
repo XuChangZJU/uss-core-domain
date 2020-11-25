175a2687c669e25aecfb3a5a7d39d1501485a274
lavystord <lavystord@gmail.com>
2020-11-25 16:48:07 +0800

    修

--- src/constraints/lens/businessDef.js
+++ src/constraints/lens/businessDef.js
@@ -265,7 +265,7 @@ const AUTH_MATRIX = {
                     '#data': [                 // 表示对现有对象或者用户的数据有要求，可以有多项，每项之间是AND的关系
                         {
                             check: ({user, row}) => {
-                                return !row.diagnosis.userId && new Date().setHours(23, 59) - (row._createAt_ || row.createAt) < 86400000;
+                                return row.diagnosis && !row.diagnosis.userId && new Date().setHours(23, 59) - (row._createAt_ || row.createAt) < 86400000;
                             },
                         }
                     ],
