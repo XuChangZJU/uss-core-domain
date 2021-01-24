273726307265f6c9fe1772a0c8957e2af15d7299
Xc <thewraith@21cn.com>
2021-01-24 20:57:09 +0800

    randomString

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -37,7 +37,7 @@ function random(len = 32) {
     const DICT = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
 　　const maxPos = DICT.length;
 　　let pwd = '';
-　　for (i = 0; i < len; i++) {
+　　for (let i = 0; i < len; i++) {
 　　　　pwd += DICT.charAt(Math.floor(Math.random() * maxPos));
 　　}
 　　return pwd;
