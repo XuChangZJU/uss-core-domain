793bb3d7586f1cc773c964803ef68ccb5f1082e5
wjr <wenjiarui1@126.com>
2021-01-29 14:22:06 +0800

    'bannerupdate'

--- src/constraints/vendue/businessDef.js
+++ src/constraints/vendue/businessDef.js
@@ -2479,6 +2479,23 @@ const AUTH_MATRIX = {
                 }
             ],
         },
+        [bannerAction.update]: {
+            auths: [
+                {
+                    '#exists': [
+                        {
+                            relation: 'userAuctionHouse',
+                            condition: ({user}) => {
+                                const query = {
+                                    userId: user.id,
+                                };
+                                return query;
+                            },
+                        },
+                    ],
+                }
+            ],
+        },
         [bannerAction.remove]: {
             auths: [
                 {
