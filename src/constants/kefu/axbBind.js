87104e6875a29ff38255d2f1873ba6773fa9adc8
邢龙 <2435409107@qq.com>
2021-02-03 19:27:53 +0800

    axbBind，删除bind动作，添加unbind动作，添加unbinded状态

--- src/constants/kefu/axbBind.js
+++ src/constants/kefu/axbBind.js
@@ -11,10 +11,10 @@ const origin = {
 };
 
 const state = Object.assign({}, commonState, {
-    // init
     binded: 301,
     inCall: 302,
     endCall: 303,
+    unbinded: 304
     // expired
 });
 
@@ -23,32 +23,36 @@ const decodeState = (s) => {
         [state.binded]: '已绑定',
         [state.inCall]: '通话中',
         [state.endCall]: '通话已结束',
+        [state.unbinded]: '已解绑',
     };
     return S[s] || decodeCommonState(s);
 };
 
 const action = Object.assign({}, commonAction, {
     // create
-    bind: 1101,
+    // bind: 1101,
     call: 1102,
     disconnect: 1103,
+    unbind: 1104,
     // expire
 });
 
 const decodeAction = (a) => {
     const A = {
-        [action.bind]: '绑定',
+        // [action.bind]: '绑定',
         [action.call]: '呼叫',
         [action.disconnect]: '挂机',
+        [action.unbind]: '解绑'
     };
     return A[a] || decodeCommonAction(a);
 };
 
 const STATE_TRANS_MATRIX = {
-    [action.bind]: [state.init, state.binded],
+    // [action.bind]: [state.init, state.binded],
     [action.call]: [state.binded, state.inCall],
     [action.disconnect]: [state.inCall, state.endCall],
     [action.expire]: [state.disconnected, state.expired],
+    [action.unbind]: [[state.binded, state.inCall, state.disconnected, state.expired], state.unbinded],
 };
 
 module.exports = {
