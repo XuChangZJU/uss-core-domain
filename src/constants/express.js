ab14ca0036a0038e152f6568351264fb5bd1da71
wjr <wenjiarui1@126.com>
2021-04-09 13:56:39 +0800

    'fix'

--- src/constants/express.js
+++ src/constants/express.js
@@ -1,7 +1,24 @@
 const {
-    TRANSPORT_STATE_TRANS_MATRIX
+    TRANSPORT_STATE_TRANS_MATRIX,
+    action: commonAction,
+    transportAction,
 } = require('./action');
+
+const { AllowEveryoneAuth } = require('../constraints/action');
+
+const action = Object.assign(
+    {}, transportAction, commonAction,
+);
+
+const AUTH_MATRIX = {
+    [action.create]: AllowEveryoneAuth,
+    [action.taSend]: AllowEveryoneAuth,
+    [action.taAccept]: AllowEveryoneAuth,
+    [action.taReject]: AllowEveryoneAuth,
+    [action.taAbort]: AllowEveryoneAuth,
+}
 
 module.exports = {
     STATE_TRANS_MATRIX: TRANSPORT_STATE_TRANS_MATRIX,
+    AUTH_MATRIX,
 }
