fa782739eac97dbd36560018d832f2de1de2286a
wjr <wenjiarui1@126.com>
2021-07-05 20:12:30 +0800

    'fix'

--- src/constants/lens/schedule.js
+++ src/constants/lens/schedule.js
@@ -4,9 +4,24 @@ const {
     decodeAction,
 } = require('../action');
 
+const type = {
+    askForLeave: 1,
+    workOvertime: 2,
+    outside: 3,
+}
 
+const decodeType = (t) => {
+    const T = {
+        [type.askForLeave]: '请假',
+        [type.workOvertime]: '加班',
+        [type.outside]: '外勤',
+    }
+    return T[t];
+}
 
 module.exports = {
     action,
     decodeAction,
+    type,
+    decodeType,
 };
