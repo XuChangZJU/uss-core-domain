10ecc1fffd1727d6a12ef73fdb6d1247df7aa3c5
wkj <278599135@qq.com>
2021-08-30 15:44:41 +0800

    文字修改

--- src/constraints/vendue/businessDef.js
+++ src/constraints/vendue/businessDef.js
@@ -2579,7 +2579,7 @@ const AUTH_MATRIX = {
                     '#exists': [DepositExistsPaddleVendue, {
                         relation: 'paddle',
                         needData: true,
-                        message: '同步拍保证金余额需大于1000元',
+                        message: '同步拍保证金余额需大于2000元',
                         condition: ({ user, actionData }) => {
                             const { deposit } = actionData;
                             const { paddleId, price } = deposit;
